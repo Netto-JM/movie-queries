42d6e2b0fe4a4daf2c32813f0a6c834040db4579
NettoJM <nettojm86@gmail.com>
2023-11-19 20:55:32 -0300

    Removes unused comments

--- src/main/java/com/betrybe/consultafilmes/Queries.java
+++ src/main/java/com/betrybe/consultafilmes/Queries.java
@@ -1,13 +1,7 @@
 package com.betrybe.consultafilmes;
 
-import static java.util.Collections.emptyList;
-import static java.util.Collections.emptyMap;
-import static java.util.Collections.emptySet;
-
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
